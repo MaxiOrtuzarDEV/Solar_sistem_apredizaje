e7a15a5361b9f132b3936a3fd2297af348b8cc9c
MaxiDev <m.ortuzar01@ufromail.cl>
2025-07-23 14:32:02 -0400

    Subida del proyecto sin carpeta de compilación

--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    if (!glfwInit()) {
+        std::cerr << "Error: No se pudo inicializar GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* ventana = glfwCreateWindow(800, 600, "Solar Sistem", nullptr, nullptr);
+    if (!ventana) {
+        std::cerr << "Error: No se pudo crear la ventana" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(ventana);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Error: No se pudo inicializar GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    while (!glfwWindowShouldClose(ventana)) {
+        glClearColor(0.1f, 0.1f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(ventana);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(ventana);
+    glfwTerminate();
     return 0;
 }
