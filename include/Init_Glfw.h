154bb3a7ffe784fb7bc1fb989c1aeca322b1aa43
MaxiDev <m.ortuzar01@ufromail.cl>
2025-07-23 14:40:29 -0400

    init glfw

---
+++ include/Init_Glfw.h
@@ -0,0 +1,16 @@
+//
+// Created by Usuario on 23-07-2025.
+//
+
+#ifndef INIT_GLFW_H
+#define INIT_GLFW_H
+
+
+
+class Init_Glfw {
+
+};
+
+
+
+#endif //INIT_GLFW_H
---
+++ src/Init_Glfw.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Usuario on 23-07-2025.
+//
+
+#include "Init_Glfw.h"
